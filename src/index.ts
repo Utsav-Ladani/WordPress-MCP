8de5db669a53eef5b3745be769e6cb24091226a9
Utsav-Ladani <201901076@daiict.ac.in>
2025-04-04 22:49:59 +0530

    Make package executable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { ServerOptions } from "@modelcontextprotocol/sdk/server/index.js";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
